be7fed5eb96503a638fe8ab98809d821e7513544
Adedayo Sule-odu <suleodu.adedayo@gmail.com>
2018-05-09 13:22:36 +0100

    Filter Added to users management view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,80 +1,110 @@
 @extends('layouts.app')
 
 @section('app_content')
+
 <section class="row component-section">
     <!-- Basic Bootstrap Table code and example -->
+    <div class="col-md-10">
+        <!--Accordion with Icons code and example  -->
+        <div class="component-box">
+            <!--Accordion with icons example -->
+            <div class="panel-group pmd-accordion" id="accordion3" role="tablist" aria-multiselectable="true"> 
+                <div class="panel"> 
+                    <div class="panel-heading" role="tab" id="headingOne">
+                        <h4 class="panel-title"> 
+                            <a data-toggle="collapse" data-parent="#accordion3" href="#collapseOne3" aria-expanded="false" aria-controls="collapseOne3" data-expandable="false" class="collapsed">
+                                <i class="material-icons pmd-sm pmd-accordion-icon-left">mood</i> Filter User <i class="material-icons md-dark pmd-sm pmd-accordion-arrow">keyboard_arrow_down</i>
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne" aria-expanded="false" style="height: 0px;">
+                        <div class="panel-body">
+                            <form class="row" role="form">
+                                <div class="col-sm-3">
+                                    <div class="form-group pmd-textfield pmd-textfield-floating-label">
+                                        <label>Filter by Name</label>
+                                        <input class="form-control" id="exampleInputEmail2"  type="text"><span class="pmd-textfield-focused"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group pmd-textfield pmd-textfield-floating-label">
+                                        <label>Filter by E-mail</label>
+                                        <input class="form-control" id="exampleInputEmail2" type="email"><span class="pmd-textfield-focused"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group pmd-textfield pmd-textfield-floating-label">
+                                        <label>Filter by Phone</label>
+                                        <input class="form-control" id="exampleInputEmail2" type="email"><span class="pmd-textfield-focused"></span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3">
+                                    <div class="form-group pmd-textfield pmd-textfield-floating-label">       
+                                        <label>Filter by Sex</label>
+                                        <select class="select-simple form-control pmd-select2">
+                                            <option></option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-sm-3    ">
+                                    <button type="submit" class="btn btn-primary pmd-ripple-effect">Search</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <!--Accordion with icons example end -->
+
+        </div> <!--Accordion with Icons code and example  end-->
+    </div>
+    <div class="col-md-2">
+        <button type="button" class="btn btn-primary"> Add New User </button>
+    </div>
     <div class="col-md-12">
         <div class="component-box">
             <!-- Basic Bootstrap Table example -->
             <div class="pmd-card pmd-z-depth pmd-card-custom-view">
                 <div class="table-responsive">
-                    <table class="table" id="table-bootstrap" cellspacing="0" cellpadding="0">
+                    <table class="table" id="table-bootstrap " cellspacing="0" cellpadding="0">
                         <thead>
                             <tr>
-                                <th>Name</th>
-                                <th>Code</th>
-                                <th>State/City <span class="caret shoarting"></span> </th>
-                                <th>End Date of Work</th>
+                                <th>#</th>
+                                <th>Full Name</th>
+                                <th>Email<span class="caret shoarting"></span> </th>
+                                <th>phone</th>
+                                <th>Sex</th>
+                                <th>DoB</th>
                                 <th>Status</th>
-                                <th>Timesheet</th>
                                 <th></th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
+                                <td>1</td>
                                 <td>Giacomo Guilizzoni</td>
                                 <td>JONEA140</td>
                                 <td>Melbourne</td>
                                 <td>1 June 2014</td>
                                 <td>Active</td>
                                 <td>Timesheet</td>
-                                <td><a href="javascript:void(0);"><i class="material-icons md-dark pmd-sm">more_vert</i></a></td>
+                                <td>
+                                    <span class="dropdown pmd-dropdown clearfix">
+                                        <button class="btn btn-sm pmd-btn-fab pmd-btn-flat pmd-ripple-effect btn-primary" type="button" id="dropdownMenuBottomRight" data-toggle="dropdown" aria-expanded="false"><i class="material-icons pmd-sm">more_vert</i></button>
+                                        <div class="pmd-dropdown-menu-container"><div class="pmd-dropdown-menu-bg pmd-dropdown-menu-bg-right"></div><ul aria-labelledby="dropdownMenuDivider" role="menu" class="dropdown-menu dropdown-menu-right pm-ini" style="clip: rect(0px, 160px, 0px, 160px);">
+                                                <li role="presentation"><a href="javascript:void(0);" tabindex="-1" role="menuitem">Action</a></li>
+                                                <li role="presentation"><a href="javascript:void(0);" tabindex="-1" role="menuitem">Another action</a></li>
+                                                <li role="presentation"><a href="javascript:void(0);" tabindex="-1" role="menuitem">Something else here</a></li>
+                                                <li class="divider" role="presentation"></li>
+                                                <li role="presentation"><a href="javascript:void(0);" tabindex="-1" role="menuitem">Separated link</a></li>
+                                            </ul></div>
+                                    </span>
+                                </td>
                             </tr>
-                            <tr>
-                                <td>Giacomo Guilizzoni</td>
-                                <td>JONEA140</td>
-                                <td>Melbourne</td>
-                                <td>1 June 2014</td>
-                                <td>Active</td>
-                                <td>Timesheet</td>
-                                <td><a href="javascript:void(0);"><i class="material-icons md-dark pmd-sm">more_vert</i></a></td>
-                            </tr>
-                            <tr>
-                                <td>Giacomo Guilizzoni</td>
-                                <td>JONEA140</td>
-                                <td>Melbourne</td>
-                                <td>1 June 2014</td>
-                                <td>Active</td>
-                                <td>Timesheet</td>
-                                <td><a href="javascript:void(0);"><i class="material-icons md-dark pmd-sm">more_vert</i></a></td>
-                            </tr>
-                            <tr>
-                                <td>Giacomo Guilizzoni</td>
-                                <td>JONEA140</td>
-                                <td>Melbourne</td>
-                                <td>1 June 2014</td>
-                                <td>Active</td>
-                                <td>Timesheet</td>
-                                <td><a href="javascript:void(0);"><i class="material-icons md-dark pmd-sm">more_vert</i></a></td>
-                            </tr>
-                            <tr>
-                                <td>Giacomo Guilizzoni</td>
-                                <td>JONEA140</td>
-                                <td>Melbourne</td>
-                                <td>1 June 2014</td>
-                                <td>Active</td>
-                                <td>Timesheet</td>
-                                <td><a href="javascript:void(0);"><i class="material-icons md-dark pmd-sm">more_vert</i></a></td>
-                            </tr>
-                            <tr>
-                                <td>Giacomo Guilizzoni</td>
-                                <td>JONEA140</td>
-                                <td>Melbourne</td>
-                                <td>1 June 2014</td>
-                                <td>Active</td>
-                                <td>Timesheet</td>
-                                <td><a href="javascript:void(0);"><i class="material-icons md-dark pmd-sm">more_vert</i></a></td>
-                            </tr>
+                            
                         </tbody>
                     </table>
                 </div>
@@ -87,9 +117,44 @@
 @push('style')
 <link rel="stylesheet" type="text/css" href="{{asset('components/file-upload/css/upload-file.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('components/file-upload/css/image-upload.css')}}">
+
+<!-- Propeller date time picker css-->
+<link rel="stylesheet" type="text/css" href="{{asset('components/datetimepicker/css/bootstrap-datetimepicker.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('components/datetimepicker/css/pmd-datetimepicker.css')}}" />
+
+<!-- Select2 css-->
+<link rel="stylesheet" type="text/css" href="{{asset('components/select2/css/select2.min.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('components/select2/css/select2-bootstrap.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{asset('components/select2/css/pmd-select2.css')}}" />
 @endpush
 
 @push('script')
 
 <script src="{{asset('components/file-upload/js/upload-image.js')}}"></script>
+<script type="text/javascript" src="{{asset('components/select2/js/select2.full.js')}}"></script>
+<!-- Propeller Select2 -->
+<script type="text/javascript">
+	$(document).ready(function() {
+		<!-- Simple Selectbox -->
+		$(".select-simple").select2({
+			theme: "bootstrap",
+			minimumResultsForSearch: Infinity,
+		});
+		<!-- Selectbox with search -->
+		$(".select-with-search").select2({
+			theme: "bootstrap"
+		});
+		<!-- Select Multiple Tags -->
+		$(".select-tags").select2({
+			tags: false,
+			theme: "bootstrap",
+		});
+		<!-- Select & Add Multiple Tags -->
+		$(".select-add-tags").select2({
+			tags: true,
+			theme: "bootstrap",
+		});
+	});
+</script>
+<script type="text/javascript" src="{{asset('components/select2/js/pmd-select2.js')}}"></script>
 @endpush
